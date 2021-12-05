86f440d69d5220f9608dfc268eefb5728418c5d2
Mykolas Blažonis <mykolaas@gmail.com>
2021-12-05 22:41:19 +0200

    Add day 5 code

---
+++ 05.py
@@ -0,0 +1,49 @@
+import numpy as np
+from aocd import get_data
+
+data = get_data(day=5)
+in_1 = data.split("\n")
+
+proc = [
+    [tuple([int(z) for z in y.split(",")]) for y in x.split(" -> ")]
+    for x in in_1
+]
+
+lines = np.zeros((len(proc), 1000, 1000), dtype=int)
+
+for i in range(len(proc)):
+    ax = proc[i][0][0]
+    ay = proc[i][0][1]
+    bx = proc[i][1][0]
+    by = proc[i][1][1]
+    ln = max(abs(bx - ax), abs(by - ay)) + 1
+    dir_x = np.sign(bx - ax)
+    dir_y = np.sign(by - ay)
+    if dir_x != 0 and dir_y != 0:
+        continue
+    for s in range(ln):
+        lines[i, ax + s * dir_x, ay + s * dir_y] = 1
+
+print(sum(sum(sum(lines) >= 2)))
+
+in_2 = in_1
+
+proc = [
+    [tuple([int(z) for z in y.split(",")]) for y in x.split(" -> ")]
+    for x in in_2
+]
+
+lines = np.zeros((len(proc), 1000, 1000), dtype=int)
+
+for i in range(len(proc)):
+    ax = proc[i][0][0]
+    ay = proc[i][0][1]
+    bx = proc[i][1][0]
+    by = proc[i][1][1]
+    ln = max(abs(bx - ax), abs(by - ay)) + 1
+    dir_x = np.sign(bx - ax)
+    dir_y = np.sign(by - ay)
+    for s in range(ln):
+        lines[i, ax + s * dir_x, ay + s * dir_y] = 1
+
+print(sum(sum(sum(lines) >= 2)))
